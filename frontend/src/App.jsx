0fac0544e8c3ce5e601ceaab7eef7d9ea9b1603b
minnewanka <philippe.inthavong@gmail.com>
2020-03-17 20:31:56 +0900

    add call to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 import './App.scss';
 import Navbar from './components/navbar/Navbar';
+import { getAllPost } from '../src/services/postService';
+import Post from './components/post/Post';
 
-function App() {
+const App = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    (async function loadData() {
+      const results = await getAllPost();
+      console.log('result', results);
+      setPosts(results);
+    })();
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
+      <div className="container">
+        {posts && posts.map(post => <Post {...post} />)}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
